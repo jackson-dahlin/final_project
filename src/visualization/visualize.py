c6a00f32bd6a0f4472fbb38d62df48eef2401a7f
jackson-dahlin <jackson_dahlin@taylor.edu>
2024-12-11 18:52:04 -0500

    Update and rename dt.py to src/visualization/visualize.py

---
+++ src/visualization/visualize.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#All required libraries are imported here for you.
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix, ConfusionMatrixDisplay, roc_curve, auc
+from sklearn.preprocessing import StandardScaler
+
+
+# Plot dt data using Seaborn
+plt.figure(figsize=(8, 6))
+sns.heatmap(dt_cm, annot=True, fmt='d', cmap='Greens', xticklabels=['Negative', 'Positive'], yticklabels=['Negative', 'Positive'])
+plt.xlabel('Predicted Labels')
+plt.ylabel('True Labels')
+plt.title('Decision Tree Confusion Matrix')
+plt.show()
+
+# Plot knn data using Seaborn
+plt.figure(figsize=(8, 6))
+sns.heatmap(knn_cm, annot=True, fmt='d', cmap='Greens', xticklabels=['Negative', 'Positive'], yticklabels=['Negative', 'Positive'])
+plt.xlabel('Predicted Labels')
+plt.ylabel('True Labels')
+plt.title('KNN Confusion Matrix')
+plt.show()
+
+# Plot nb data using Seaborn
+plt.figure(figsize=(8, 6))
+sns.heatmap(nb_cm, annot=True, fmt='d', cmap='Greens', xticklabels=['Negative', 'Positive'], yticklabels=['Negative', 'Positive'])
+plt.xlabel('Predicted Labels')
+plt.ylabel('True Labels')
+plt.title('Naive Bayes Confusion Matrix')
+plt.show()
+
+
+# In[10]:
+
+
+# Plot the decision tree
+plt.figure(figsize = (12,8))
+plot_tree(DTmodel, feature_names = features, class_names = ['No Donation', 'Donation'], filled = True)
+plt.title(f'Decision Tree (Features: {features}, Max Depth: {depth_limit})')
+plt.show()
+
+# Calculate the FPR, TPR, and thresholds for decision tree
+fpr, tpr, thresholds = roc_curve(y_valid, dt_pred_valid)
+
+# Calculate the AUC (Area Under the Curve)
+roc_auc = auc(fpr, tpr)
+
+# Plot the ROC curve for decision tree
+plt.figure()
+plt.plot(fpr, tpr, color = 'green', label = f'ROC curve (AUC = {roc_auc:.2f})')
+plt.plot([0, 1], [0, 1], color = 'black')  # Diagonal line
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.0])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Validation Decision Tree ROC Curve')
+plt.legend(loc = 'lower right')
+plt.grid()
+plt.show()
+
+# Calculate the FPR, TPR, and thresholds for kBB
+fpr, tpr, thresholds = roc_curve(y_valid, knn_pred_valid)
+
+# Calculate the AUC (Area Under the Curve)
+roc_auc = auc(fpr, tpr)
+
+# Plot the ROC curve
+plt.figure()
+plt.plot(fpr, tpr, color = 'navy', label = f'ROC curve (AUC = {roc_auc:.2f})')
+plt.plot([0, 1], [0, 1], color = 'black')  # Diagonal line
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.0])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('k-NN ROC Curve')
+plt.legend(loc = 'lower right')
+plt.grid()
+plt.show()
+
+# Calculate the FPR, TPR, and thresholds
+fpr, tpr, thresholds = roc_curve(y_valid, nb_pred_valid)
+
+# Calculate the AUC (Area Under the Curve)
+roc_auc = auc(fpr, tpr)
+
+# Plot the ROC curve
+plt.figure()
+plt.plot(fpr, tpr, color = 'red', label = f'ROC curve (AUC = {roc_auc:.2f})')
+plt.plot([0, 1], [0, 1], color = 'black')  # Diagonal line
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.0])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Naive Bayes ROC Curve')
+plt.legend(loc = 'lower right')
+plt.grid()
+plt.show()
