d6d39f3cd35272b2617e2df8b05b74a30fcdfe42
jackson-dahlin <jackson_dahlin@taylor.edu>
2024-12-11 18:40:29 -0500

    Update evaluate_model.py

--- src/models/evaluate_model.py
+++ src/models/evaluate_model.py
@@ -47,3 +47,36 @@ knn_pred_valid = knn_fit.predict(X_valid)
 
 # Predict knn on test data
 knn_pred_test = knn_fit.predict(X_test)
+
+# Creating a confusion matrix for the dt validating data
+dt_tn, dt_fp, dt_fn, dt_tp = confusion_matrix(y_valid, dt_pred_valid).ravel()
+dt_cm = confusion_matrix(y_valid, dt_pred_valid)
+
+# Creating a confusion matrix for the knn validating data
+knn_tn, knn_fp, knn_fn, knn_tp = confusion_matrix(y_valid, knn_pred_valid).ravel()
+knn_cm = confusion_matrix(y_valid, knn_pred_valid)
+
+# Creating a confusion matrix for the nb validating data
+nb_tn, nb_fp, nb_fn, nb_tp = confusion_matrix(y_valid, nb_pred_valid).ravel()
+nb_cm = confusion_matrix(y_valid, nb_pred_valid)
+
+# Evaluate the model on dt valdiating data
+dt_accuracy = metrics.accuracy_score(y_valid, dt_pred_valid)
+dt_precision = precision_score(y_valid, dt_pred_valid)
+dt_recall = metrics.recall_score(y_valid, dt_pred_valid)
+dt_specificity = dt_tn / (dt_tn + dt_fp)
+dt_f1 = metrics.f1_score(y_valid, dt_pred_valid)
+
+# Evaluate the model on knn valdiating data
+knn_accuracy = metrics.accuracy_score(y_valid, knn_pred_valid)
+knn_precision = precision_score(y_valid, knn_pred_valid)
+knn_recall = metrics.recall_score(y_valid, knn_pred_valid)
+knn_specificity = knn_tn / (knn_tn + knn_fp)
+knn_f1 = metrics.f1_score(y_valid, knn_pred_valid)
+
+# Evaluate the model on nb valdiating data
+nb_accuracy = metrics.accuracy_score(y_valid, nb_pred_valid)
+nb_precision = precision_score(y_valid, nb_pred_valid)
+nb_recall = metrics.recall_score(y_valid, nb_pred_valid)
+nb_specificity = nb_tn / (nb_tn + nb_fp)
+nb_f1 = metrics.f1_score(y_valid, nb_pred_valid)
