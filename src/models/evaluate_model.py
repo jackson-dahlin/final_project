c07a94a3185edcb21ae0f2f2e0e3646e69d9f174
jackson-dahlin <jackson_dahlin@taylor.edu>
2024-12-11 20:34:23 -0500

    Update evaluate_model.py

--- src/models/evaluate_model.py
+++ src/models/evaluate_model.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 #All required libraries are imported here for you.
 import pandas as pd
 import numpy as np
@@ -21,32 +15,30 @@ from sklearn.pipeline import make_pipeline
 from sklearn.metrics.pairwise import euclidean_distances
 from sklearn import set_config
 
-# Predict DT on train data
+# Predict DT
 dt_pred_train = DTmodel.predict(X_train[features])
-
-# Predict DT on validation data
 dt_pred_valid = DTmodel.predict(X_valid[features])
-
-# Predict DT on test data
 dt_pred_test = DTmodel.predict(X_test[features])
 
-# Predict NB on train data
+# Predict NB
 nb_pred_train = NBmodel.predict(X_train[features])
-
-# Predict NB on validating data
 nb_pred_valid = NBmodel.predict(X_valid[features])
-
-# Predict NB on test data
 nb_pred_test = NBmodel.predict(X_test[features])
 
-# Predict knn on train data
+# Predict knn
 knn_pred_train = knn_fit.predict(X_train)
-
-# Predict knn on validating data
 knn_pred_valid = knn_fit.predict(X_valid)
-
-# Predict knn on test data
 knn_pred_test = knn_fit.predict(X_test)
+
+# Predict log-NB
+lnb_pred_train = NBmodel.predict(X1_train[features])
+lnb_pred_valid = NBmodel.predict(X1_valid[features])
+lnb_pred_test = NBmodel.predict(X1_test[features])
+
+# Predict log-knn
+lknn_pred_train = knn_fit.predict(X1_train)
+lknn_pred_valid = knn_fit.predict(X1_valid)
+lknn_pred_test = knn_fit.predict(X1_test)
 
 # Creating a confusion matrix for the dt validating data
 dt_tn, dt_fp, dt_fn, dt_tp = confusion_matrix(y_valid, dt_pred_valid).ravel()
@@ -59,6 +51,14 @@ knn_cm = confusion_matrix(y_valid, knn_pred_valid)
 # Creating a confusion matrix for the nb validating data
 nb_tn, nb_fp, nb_fn, nb_tp = confusion_matrix(y_valid, nb_pred_valid).ravel()
 nb_cm = confusion_matrix(y_valid, nb_pred_valid)
+
+# Creating a confusion matrix for the log knn validating data
+knn_tn, knn_fp, knn_fn, knn_tp = confusion_matrix(y_valid, lknn_pred_valid).ravel()
+lknn_cm = confusion_matrix(y_valid, lknn_pred_valid)
+
+# Creating a confusion matrix for the log nb validating data
+nb_tn, nb_fp, nb_fn, nb_tp = confusion_matrix(y_valid, lnb_pred_valid).ravel()
+lnb_cm = confusion_matrix(y_valid, lnb_pred_valid)
 
 # Evaluate the model on dt valdiating data
 dt_accuracy = metrics.accuracy_score(y_valid, dt_pred_valid)
@@ -80,3 +80,17 @@ nb_precision = precision_score(y_valid, nb_pred_valid)
 nb_recall = metrics.recall_score(y_valid, nb_pred_valid)
 nb_specificity = nb_tn / (nb_tn + nb_fp)
 nb_f1 = metrics.f1_score(y_valid, nb_pred_valid)
+
+# Evaluate the model on log knn valdiating data
+lknn_accuracy = metrics.accuracy_score(y_valid, lknn_pred_valid)
+lknn_precision = precision_score(y_valid, lknn_pred_valid)
+lknn_recall = metrics.recall_score(y_valid, lknn_pred_valid)
+lknn_specificity = lknn_tn / (lknn_tn + lknn_fp)
+lknn_f1 = metrics.f1_score(y_valid, lknn_pred_valid)
+
+# Evaluate the model on log nb valdiating data
+lnb_accuracy = metrics.accuracy_score(y_valid, lnb_pred_valid)
+lnb_precision = precision_score(y_valid, lnb_pred_valid)
+lnb_recall = metrics.recall_score(y_valid, lnb_pred_valid)
+lnb_specificity = lnb_tn / (lnb_tn + lnb_fp)
+lnb_f1 = metrics.f1_score(y_valid, lnb_pred_valid)
